921a0f287b7df618eb5502d147bbe1348ffca654
Baudouin Douchet <67335737+BDouchet@users.noreply.github.com>
2021-01-27 15:22:37 +0100

    Create vgg.py

---
+++ models/vgg.py
@@ -0,0 +1,44 @@
+# VGG network 
+# 9 convolutional layers
+
+from tensorflow.keras.layers import Conv1D, Dropout, BatchNormalization, AveragePooling1D, Reshape, Flatten
+
+
+def VGG(units,kernel=5,dropout=0.0,bn=True,channel=3):
+    model=models.Sequential()
+
+    model.add(Conv1D(units, kernel,input_shape=(168,10),activation='linear',padding='same'))
+    if bn:
+        model.add(BatchNormalization())
+    model.add(Dropout(dropout))
+    model.add(Conv1D(units, kernel,activation='linear',padding='same'))
+    if bn:
+        model.add(BatchNormalization())
+    model.add(Dropout(dropout))
+    model.add(AveragePooling1D())
+
+    model.add(Conv1D(units*2, kernel,activation='linear',padding='same'))
+    if bn:
+        model.add(BatchNormalization())
+    model.add(Dropout(dropout))
+    model.add(Conv1D(units*2, kernel,activation='linear',padding='same'))
+    if bn:
+        model.add(BatchNormalization())
+    model.add(Dropout(dropout))
+    model.add(AveragePooling1D())
+
+    model.add(Conv1D(units*4, kernel,activation='linear',padding='same'))
+    if bn:
+        model.add(BatchNormalization())
+    model.add(Dropout(dropout))
+    model.add(Conv1D(units*4, kernel,activation='linear',padding='same'))
+    if bn:
+        model.add(BatchNormalization())
+    model.add(Dropout(dropout))
+    model.add(Conv1D(units,1,activation='linear',padding='same'))
+    model.add(Flatten())
+    model.add(Dense(units*4,activation='linear'))
+    model.add(Dense(48*3,activation='linear'))
+    model.add(Reshape(target_shape=(48,channel)))
+    
+    return model
